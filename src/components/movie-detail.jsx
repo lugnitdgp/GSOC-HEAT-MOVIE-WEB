957ac22585e9b56a687e14883016ebfbd366cf11
Shru-Singh <gshrutisingh@gmail.com>
2021-05-26 19:23:16 +0530

    details page done

--- src/components/movie-detail.jsx
+++ src/components/movie-detail.jsx
@@ -37,30 +37,22 @@ const Details = () => {
       <img
         src={`${process.env.REACT_APP_IMAGE_URL}${movie.backdrop_path}`}
         alt=""
-        style={{
-          objectFit: "cover",
-          width: "100vw",
-          height: "100vh",
-          position: "fixed",
-          zIndex: 1,
-        }}
+        className="backgroundimage"
       />
       <div
         className="tails"
-        style={{
-          // backgroundImage: `url(
-          //   https://image.tmdb.org/t/p/original${movie.backdrop_path}
-          // )`,
-          // backgroundAttachment: "fixed",
-          // backgroundPosition: "center",
-          // backgroundSize: "cover",
-          // width: "100vw",
-          // marginTop: "0px",
-          backgroundColor: "rgba(0, 0, 0, 0.2)",
-          backdropFilter: "blur(5px)",
-          position: "relative",
-          zIndex: 2,
-        }}
+        style={
+          {
+            // backgroundImage: `url(
+            //   https://image.tmdb.org/t/p/original${movie.backdrop_path}
+            // )`,
+            // backgroundAttachment: "fixed",
+            // backgroundPosition: "center",
+            // backgroundSize: "cover",
+            // width: "100vw",
+            // marginTop: "0px",
+          }
+        }
       >
         <div className="populardetails">
           <div className="detailsimage">
@@ -72,28 +64,25 @@ const Details = () => {
           <div className="align">
             <div className="details">
               <div className="detailstitle">
-                <h2 style={{ fontSize: "50px" }}>{movie.original_title}</h2>
+                <h1>{movie.original_title}</h1>
               </div>
-              <div className="detailstitle" style={{ display: "flex" }}>
+              <div className="desc">
+                <div className="detailsdesc">
+                  <h2>{movie.overview}</h2>
+                </div>
+              </div>
+              <div className="detailsrating">
                 <h2 style={{ marginRight: "10px" }}>Rating: </h2>
-                <h2>{movie.vote_average}</h2>
-                <span
-                  style={{
-                    fontSize: "170%",
-                    color: "yellow",
-                    marginTop: "18px",
-                  }}
-                >
-                  &#9733;
-                </span>
+                <h2>{movie.vote_average} &#11088;</h2>
+                {/* <span className="span"></span> */}
               </div>
-              <div className="detailstitle">
-                <h2>{movie.overview}</h2>
+              <div className="detailsruntime">
+                <h2>
+                  Runtime: {Math.floor(movie.runtime / 60)}h{" "}
+                  {movie.runtime % 60}min
+                </h2>
               </div>
-              <div className="detailstitle">
-                <h2>Runtime: {movie.runtime}</h2>
-              </div>
-              <div className="detailstitle">
+              <div className="detailsrelease">
                 <h2>Released: {movie.release_date}</h2>
               </div>
 
@@ -106,19 +95,23 @@ const Details = () => {
             </div>
           </div>
         </div>
+
         <div className="videos">
-          {video?.map((vid) => (
-            <iframe
-              className="video"
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/${vid.key}`}
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-          ))}
+          <h1>Trailer Videos</h1>
+          <div className="videoscroll">
+            <div className="video">
+              {video?.map((vid) => (
+                <iframe
+                  className="vid"
+                  src={`https://www.youtube.com/embed/${vid.key}`}
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
