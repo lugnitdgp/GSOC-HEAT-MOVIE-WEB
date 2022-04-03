193b7af1ae2b5c9247d47f1ec55c40ee20b6ede5
Rajdeep Chowdhury <rajdeepchowdhury318@gmail.com>
2022-04-03 17:22:47 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./styles/app.css";
 import ReactRouterSetup from "../src/Router";
-const App = () => {
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`
+
+function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
+  return (
+    <div>
+      <ReactRouterSetup />
+      <br></br>
+      <br></br>
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <GlobalStyles />
+        <StyledApp>
+          <label className="toggle">
+            <input type="checkbox" onChange={() => themeToggler()}></input>
+            <span className="slider"></span>
+            <br></br>
+            <h4 className="toggle_title">Change Theme</h4>
+          </label>
+        </StyledApp>
+      </ThemeProvider>
+      <br></br>
+    </div>
+  );
+}
+/*const App = () => {
   return (
     <div>
       <ReactRouterSetup />
     </div>
   );
-};
+};*/
 
 export default App;
